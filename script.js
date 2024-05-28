5ad243c32a761bf1d7fd4257f742aa101f23012e
Andre victoria <122878923+Saint8083@users.noreply.github.com>
2024-05-28 22:43:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
-const arrows = document.querySelectorAll(".arrows");
-const list2 = document.querySelector(".list2");
-
- arrows.forEach((arrow, i)=> {
-  const itemNumber =  list2[i]
-  let  clickCounter = 0;
-  arrow.addEventListener("click", () => {
-      clickCounter++;
-      if (itemNumber - (4 + clickCounter) >= 0) {
-        list2[i].style.transform = `translateX($ {
-        list2[i].computedStyleMap().get("transform")[0].x.value - 300 
-      }px)`;
-      } else {
-        list2[i].style.transform = "translateX(0)";
-        clickCounter = 0;
-      }
-    });
-      console.log(list2[i])
+var swiper = new Swiper(".home", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  
 });
